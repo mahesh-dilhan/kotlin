b140a491414b108a42243c7ccae2be34407516ae
mahesh <mail.mahesh.lk@gmail.com>
2020-12-08 16:25:45 +0800

    ex05-class-constrctor

--- src/Ex-05.kt
+++ src/Ex-05.kt
@@ -21,10 +21,15 @@ class NestedInner {
 
     }
 }
+
+class PrintConstruct(val consstr: String){
+
+}
 fun main() {
     val printValue= PrintValue()
     printValue.printme("this is the str")
     val nestedOuter= NestedOuter.Inner().printme("this is nested str")
     val nestedInner= NestedInner().Inner().printme("this is nested inner str")
-
+    val printConstruct=PrintConstruct(" constructor")
+    println("this is : ${printConstruct.consstr}")
 }
