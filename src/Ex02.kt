02d361dbd7ff92c5ece25fad7258640875e21881
mahesh <mail.mahesh.lk@gmail.com>
2020-12-08 15:38:21 +0800

    ex02-types

---
+++ src/Ex02.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val a: Int=10
+    val b: Float=1.1f
+    val c: Double=100.0
+    val d: String="hello types"
+
+    println("Int value $a")
+    println("Float value $b")
+    println("Double value $c")
+    println("String value $d")
+}
