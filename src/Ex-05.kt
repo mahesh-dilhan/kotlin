76687fa7ecc7a666ce26f23b2bc77a07f75deb41
mahesh <mail.mahesh.lk@gmail.com>
2020-12-08 16:15:42 +0800

    ex05-class-inner

--- src/Ex-05.kt
+++ src/Ex-05.kt
@@ -12,8 +12,19 @@ class NestedOuter {
 
     }
 }
+
+class NestedInner {
+    inner class Inner{
+        fun printme(s:String) {
+            println(s);
+        }
+
+    }
+}
 fun main() {
     val printValue= PrintValue()
     printValue.printme("this is the str")
     val nestedOuter= NestedOuter.Inner().printme("this is nested str")
+    val nestedInner= NestedInner().Inner().printme("this is nested inner str")
+
 }
